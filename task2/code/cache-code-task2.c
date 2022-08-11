8d967105f506ce3264347d25276b7597dc204034
Ricky <debojeetofficial@gmail.com>
2022-08-11 23:33:30 +0530

    First running code of task2

---
+++ task2/code/cache-code-task2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define K 1024
+#define M 1024 * 1024
+
+int power(int);
+
+int main(int argc, char* argv[])
+{
+    int input = atoi(argv[1]);
+    int size = power(input);
+
+    int* arr = (int*)malloc(sizeof(int) * size);
+    for (int i = 0; i < 10; i++)
+    {
+        for(unsigned long int steps = 0; steps < 10 * M; steps++)
+        {
+            arr[(steps * 16) & (size - 1)]++;
+        }
+    }
+    free(arr);
+    return 0;
+}
+
+int power(int n)
+{
+    int i;
+    for(i = 1; i <= n; i *= 2 );
+    return i;
+}
