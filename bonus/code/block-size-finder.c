14275e301d5d77afaea6fff495bd38ae7739f111
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-08-12 03:34:38 +0530

    Added Comments

--- bonus/code/block-size-finder.c
+++ bonus/code/block-size-finder.c
@@ -4,33 +4,36 @@
 #define KB 1024
 #define MB 1024 * 1024
 
+/**
+ * This program is used to measure block size of cache.
+ *
+ * The program accesses the array at strides.
+ * The strides are powers of 2 ranging from 1 to 2048.
+ * The program generates the time taken for the different strides.
+ * @return int
+ */
 int main()
 {
-    // Initializing an integer array of size 20 MB
     static int arr[5 * MB];
-
-    // Finding mod value since for powers of 2; a % b = a & (b - 1)
     unsigned int mod = 5 * MB - 1;
 
-    // Variables for storing time related values
-    clock_t start, end;
+    clock_t start, end; ///< used to measure time taken for each stride
     double cpu_time;
 
     for (int strides = 1, powers = 0; strides <= 2048; strides *= 2, powers++)
     {
-        cpu_time = 0;
-        for (int i= 0; i < 10; i++)
+        cpu_time = 0; ///< reset the time taken for each stride
+        for (int i = 0; i < 10; i++)
         {
-            start = clock();
+            start = clock(); ///< start the timer
             for (unsigned long int steps = 0; steps < 10 * MB; steps++)
             {
-                arr[(steps * strides) & mod] = 1;
+                arr[(steps * strides) & mod] = 1; ///< since size is 2^n we can use bitwise and to get the remainder
             }
-            end = clock();
-            cpu_time += (double)(end - start) / CLOCKS_PER_SEC;
+            end = clock();                                      ///< stop the timer
+            cpu_time += (double)(end - start) / CLOCKS_PER_SEC; ///< add the time taken for each iteration
         }
-        // Average CPU time
-        cpu_time /= 10;
+        cpu_time /= 10; ///< get the average time taken for each stride
         printf("%d %.20f\n", powers, cpu_time);
     }
     return 0;
