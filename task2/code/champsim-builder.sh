ab82ab8bc12b3ff5e3e4d2acf331e3738b10315b
Ricky <debojeetofficial@gmail.com>
2022-08-11 23:35:20 +0530

    Script to generate output of champsim

---
+++ task2/code/champsim-builder.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+SIZE='
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+'
+
+CHAMPSIM_DIR=""
+TRACE_DIR=""
+DESTINATION='data'
+
+mkdir -p ${DESTINATION}
+
+for i in ${SIZE}; do
+    (./run_champsim.sh bimodal-no-no-no-no-lru-1core 1 1 ${i}.champsimtrace.xz)
+done
