85ed49a12c7b61ac9934f776c538a588b3f87cf3
Ricky <debojeetofficial@gmail.com>
2022-08-12 01:34:47 +0530

    added analysis script

---
+++ task2/code/analysis-script.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo Analysing and Generating PNG!!
+touch file.txt
+
+SIZE='
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+'
+
+touch file.txt
+
+for i in ${SIZE}; do
+    echo -n "${i} " >> file.txt
+    cat ../output/${i}.txt | grep IPC | cut -d ' ' -f 9,10 | grep IPC | cut -d ' ' -f2 >> file.txt 
+done
+
+cat file.txt | graph -T png -h .3 -w .6 > ../reports_and_plots/plot-task1.png
+
+echo Done!!
+
