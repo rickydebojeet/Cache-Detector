c9181909a5d7ae25fa995d75be51fed610faba9f
Debojeet Das <43584277+rickydebojeet@users.noreply.github.com>
2022-08-12 03:06:46 +0530

    Added comments

--- task1/code/cache-code-task1.c
+++ task1/code/cache-code-task1.c
@@ -1,4 +1,3 @@
-// Generate cache sizes of my computer
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,28 +5,51 @@
 #define K 1024
 #define M 1024 * 1024
 
+/**
+ * This program is used to measure different caches of the CPU.
+ *
+ * The program allocates arrays of different sizes and accesses them for equivalent time
+ * Checks the time taken for each array size and prints the results
+ * @return int
+ */
 int main()
 {
-    clock_t start, end;
+    clock_t start, end; ///< used to measure time taken for each array size
     double cpu_time;
-    int size;
 
-    for (int size = 1024/sizeof(int), power = 10; power <= 26; size *=2, power++)
+    /**
+     * Allocate arrays of different sizes
+     *
+     * @size: size of the array in bytes.
+     * @power: power of 2 to be used to calculate the size of the array.
+     *
+     * The loop allocates arrays of size 2^power bytes the power ranges from 10 to 24
+     * that is the array size ranges form 1KB to 16MB
+     */
+    for (int size = 1024 / sizeof(int), power = 10; power <= 24; size *= 2, power++)
     {
-        int* arr = (int *)malloc(sizeof(int) * size);
-        cpu_time = 0;
-        for(int i = 0; i < 10; i++)
+        int *arr = (int *)malloc(sizeof(int) * size);
+        cpu_time = 0; ///< reset the time taken for each array size
+
+        for (int i = 0; i < 10; i++)
         {
-            start = clock();
-            for (unsigned long int steps = 0; steps < 10 * M; steps++)
+            start = clock(); ///< start the timer
+            /**
+             * Access the array for equivalent number of times
+             *
+             * @steps: number of acceses to the array
+             *
+             * The loop accesses the array at an interval of 64 bytes
+             * so that if there is cache miss every time it is trying to access the array
+             */
+            for (unsigned long int steps = 0; steps < 20 * M; steps++)
             {
-                arr[(steps * 16) & (size - 1)]++;
+                arr[(steps * 16) & (size - 1)]++; ///< since size is 2^n we can use bitwise and to get the remainder
             }
-            end = clock();
-            cpu_time += (double)(end - start) / CLOCKS_PER_SEC;
-        
+            end = clock();                                      ///< stop the timer
+            cpu_time += (double)(end - start) / CLOCKS_PER_SEC; ///< add the time taken for each iteration
         }
-        cpu_time /= 10;
+        cpu_time /= 10; ///< get the average time taken for each array size
         printf("%d %.30f\n", power, cpu_time);
         free(arr);
     }
--- task1/code/run-script.sh
+++ task1/code/run-script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo Compiling and Generating PNG!!
+# Compile the code and generate the PNG
 gcc cache-code-task1.c && ./a.out | graph -T png -h .3 -w .6 > ../reports_and_plots/plot-task1.png
 echo Done!!
