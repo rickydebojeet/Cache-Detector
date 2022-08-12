c1a2439e5050a00b257b18817f305a19966ed257
Ricky <debojeetofficial@gmail.com>
2022-08-12 14:29:47 +0530

    The script generates two plots

--- task1/code/run-script.sh
+++ task1/code/run-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo Compiling and Generating PNG!!
-# Compile the code and generate the PNG
-gcc cache-code-task1.c && ./a.out | graph -T png -h .3 -w .6 > ../reports_and_plots/plot-task1.png
+# Generating plot till 1 MB Arrays
+gcc cache-code-task1.c && ./a.out 20 | graph -T png --bitmap-size "1400x1200" -g 3 -L "Time vs Size" -X "Size in Powers of 2" -Y "Execution Time(s)" > ../reports_and_plots/plot-task1-precise.png
+# Generating plot till 16 MB Arrays
+gcc cache-code-task1.c && ./a.out | graph -T png --bitmap-size "1400x1200" -g 3 -L "Time vs Size" -X "Size in Powers of 2" -Y "Execution Time(s)" > ../reports_and_plots/plot-task1.png
 echo Done!!
