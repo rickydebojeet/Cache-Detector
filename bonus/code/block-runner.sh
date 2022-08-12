28a2b5007f9b0924edc0b4571dad0d208baec71e
Ricky <debojeetofficial@gmail.com>
2022-08-12 16:08:41 +0530

    Beautiful plots

--- bonus/code/block-runner.sh
+++ bonus/code/block-runner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo Compiling and Generating PNG!!
-gcc block-size-finder.c && ./a.out | graph -T png -h .3 -w .6 > ../blockSize.png 
+gcc block-size-finder.c && ./a.out | graph -T png --bitmap-size "1400x1200" -g 3 -L "Time vs Stride" -X "Strides in Power of 2" -Y "Execution Time(s)" > ../blockSize.png 
 echo Done!!
