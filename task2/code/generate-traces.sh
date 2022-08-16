6862c0b27b3dcda89f5b1c0fedbd9b2367da5b5d
Ricky <debojeetofficial@gmail.com>
2022-08-16 13:55:16 +0530

    Now trace gives perferct curves

--- task2/code/generate-traces.sh
+++ task2/code/generate-traces.sh
@@ -18,12 +18,12 @@ SIZE='
 '
 
 CHAMPSIM_PATH="/home/ricky/Desktop/ChampSim/"
-EXECUTABLE_PATH="/home/ricky/Desktop/Cache-Detector/task2/code/a.out"
-TRACES_PATH="/home/ricky/Desktop/Cache-Detector/task2/traces/"
+EXECUTABLE_PATH="/home/ricky/Desktop/22D0371/task2/code/a.out"
+TRACES_PATH="/home/ricky/Desktop/22D0371/task2/traces/"
 
 # Create the traces for the different sizes of cache
 for i in ${SIZE}; do
-	./pin -t ${CHAMPSIM_PATH}tracer/obj-intel64/champsim_tracer.so -o ${i}.champsimtrace -t 1000000 -- ${EXECUTABLE_PATH} ${i}
+	./pin -t ${CHAMPSIM_PATH}tracer/obj-intel64/champsim_tracer.so -o ${i}.champsimtrace -s 10000000 -t 2000000 -- ${EXECUTABLE_PATH} ${i}
     xz ${i}.champsimtrace
 done
 
